6a615d5bbe07a08239ce742a83b9ae7aea02fcb5
SusanFair <sfair@live.ca>
2023-03-25 17:25:43 -0400

    Part3 Screen Shots

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,6 +1,7 @@
 # Challenge File
 
 # Challenge - Part 1
+#####################################
 # Load libraries
 install.packages("tidyverse")
 library(tidyverse) #call the library
@@ -13,7 +14,7 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaData)) #generate summary statistics
 
 # Challenge - Part 2
-
+####################################
 #Import CSV
 SpoilerData <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 
@@ -22,3 +23,22 @@ total_summary <- subset(SpoilerData) %>% summarize(Mean=mean(PSI), Median=median
 
 # Lot Summary
 lot_summary <- subset(SpoilerData) %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+
+# Challenge - Part 3
+##################################
+
+# T.test (Population mean = 1,500 lbs / square inch)
+populationMean <- 1500
+t.test(SpoilerData$PSI, mu=populationMean)   #pass vector of PSI to compare with population
+
+# T.test by Manufacturing Lot
+filterLot1 <- subset(SpoilerData, Manufacturing_Lot == "Lot1")
+filterLot2 <- subset(SpoilerData, Manufacturing_Lot == "Lot2")
+filterLot3 <- subset(SpoilerData, Manufacturing_Lot == "Lot3")
+
+t.test(filterLot1$PSI, mu=populationMean)   #pass vector of PSI to compare with population
+t.test(filterLot2$PSI, mu=populationMean)   #pass vector of PSI to compare with population
+t.test(filterLot1$PSI, mu=populationMean)   #pass vector of PSI to compare with population
+
+#t.test(filterLot1 <- (subset(SpoilerData, Manufacturing_Lot == "Lot1")$PSI), mu=populationMean) 
--- README.md
+++ README.md
@@ -26,3 +26,19 @@ After you’ve completed the technical analysis for each part, provide a short s
 ![Linear Regression](https://github.com/SusanFair/MechaCar_Statistical_Analysis/blob/main/Resources/LinearRegression.PNG)
 
 ![Linear Regression Summary](https://github.com/SusanFair/MechaCar_Statistical_Analysis/blob/main/Resources/LinearRegressionSummary.PNG)
+
+## Summary Statistics on Suspension Coils
+
+### Total Summary
+
+![Totals Summary](https://github.com/SusanFair/MechaCar_Statistical_Analysis/blob/main/Resources/Part2_TotalSummary.PNG)
+
+### Lot Summary
+
+![Lot Summary](https://github.com/SusanFair/MechaCar_Statistical_Analysis/blob/main/Resources/Part2_LotSummary.PNG)
+
+The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+
+## Test Tests on the Suspension Coils
+
